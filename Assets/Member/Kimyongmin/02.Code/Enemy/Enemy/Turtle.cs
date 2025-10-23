6753f7c134a1f796331129b48746ed67b7ac6be0
kimyongmin519 <qwdfbns5198@gmail.com>
2025-10-23 10:34:28 +0900

    TurtleBugFixed

--- Assets/Member/Kimyongmin/02.Code/Enemy/Enemy/Turtle.cs
+++ Assets/Member/Kimyongmin/02.Code/Enemy/Enemy/Turtle.cs
@@ -6,16 +6,29 @@ namespace Member.Kimyongmin._02.Code.Enemy.Enemy
     public class Turtle : global::Enemy
     {
         [SerializeField] private float dashPower = 10f;
-        
+        private AttackHitbox _attackHitbox;
+
+        private Vector2 _dashDir = Vector2.right;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _attackHitbox = GetComponentInChildren<AttackHitbox>();
+        }
 
         public override void Attack()
         {
             AgentMovemant.IsDashing = true;
+            StartCoroutine(_attackHitbox.ShowHitbox(Target.position));
+            Debug.Log("dd");
+            
+            _dashDir = (Target.position - transform.position).normalized;
         }
 
         public void Dash()
         {
-            AgentMovemant.RbCompo.linearVelocity = GetTarget() * dashPower;
+            
+            AgentMovemant.RbCompo.linearVelocity = _dashDir * dashPower;
             ResetCooltime();
         }
         
