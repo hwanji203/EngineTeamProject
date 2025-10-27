6b13ac2740e7746d2d9fbecac721940f8b8e45ed
changjue <aaaeip0000@gmail.com>
2025-10-27 15:06:37 +0900

    feat : stageUI

---
+++ Assets/Member/JCJ/Scripts/Overlay/ButtonManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]private Overlay_Button[] buttons;
+
+    public void ClickButon(Overlay_Button overlayButton)
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (buttons[i] != overlayButton)
+            {
+                buttons[i].OutButton();
+            }
+        }
+    }
+    
+}
