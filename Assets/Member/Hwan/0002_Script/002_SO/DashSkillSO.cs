b2ac22ba82f80a03243b45fd82ccbdd8cf239dde
Hwanji203 <hwanji203@gmail.com>
2025-11-17 14:53:49 +0900

    feat: VFS 추가

--- Assets/Member/Hwan/0002_Script/002_SO/DashSkillSO.cs
+++ Assets/Member/Hwan/0002_Script/002_SO/DashSkillSO.cs
@@ -7,13 +7,13 @@ public class DashSkillSO : PlayerSkillSO
     public override IEnumerator AttackStart(Transform playerTrn, float defaultDamage)
     {
         detectedCollider.Clear();
-
-        for (int i = 0; i < attackCount; i++)
+        CheckBox(playerTrn, defaultDamage);
+        VFXManager.Instance.Play(VFXType.DashAttack, playerTrn.position, playerTrn.rotation);
+        VFXManager.Instance.Play(VFXType.DashBoost, playerTrn.position, playerTrn.rotation);
+        for (int i = 0; i < attackCount - 1; i++)
         {
+            yield return new WaitForSeconds(AttackTime / attackCount - 1);
             CheckBox(playerTrn, defaultDamage);
-
-            if (i + 1 == attackCount) break;
-            yield return new WaitForSeconds(AttackTime / attackCount);
         }
     }
 }
