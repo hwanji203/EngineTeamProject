71b04669e5e6f944dca9a73f58c7603493503eef
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-19 08:51:01 +0900

    Boss DefaultLogic

--- Assets/Member/Kimyongmin/02.Code/Boss/NewShark/Shark.cs
+++ Assets/Member/Kimyongmin/02.Code/Boss/NewShark/Shark.cs
@@ -1,19 +1,33 @@
+using System;
+using Member.Kimyongmin._02.Code.Boss.SO;
 using UnityEngine;
 
 namespace Member.Kimyongmin._02.Code.Boss.NewShark
 {
     public class Shark : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [field:SerializeField] public SharkDataSO SharkData { get; private set; }
+        [SerializeField] private Transform target;
+        public SharkMovement SharkMovement { get; private set; }
+        private Vector3 _moveDir;
+
+        private void Awake()
         {
-        
+            SharkMovement = GetComponent<SharkMovement>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
-        
+            SharkMovement.RbMove(_moveDir, SharkData.speed);
+        }
+
+        public void SetMoveDir(Vector3 dir)
+        {
+            _moveDir = dir;
+        }
+        public Vector3 GetTargetDir()
+        {
+            return (target.position - transform.position).normalized;
         }
     }
 }
