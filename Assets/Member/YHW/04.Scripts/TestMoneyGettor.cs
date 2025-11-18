567d2f89e2e5832fcff34397a50fe77fa793db34
Yoo Hyeon-woo <hyeonwoo.ggm09@gmail.com>
2025-11-18 16:31:35 +0900

    feat : HealthBar

--- Assets/Member/YHW/04.Scripts/TestMoneyGettor.cs
+++ Assets/Member/YHW/04.Scripts/TestMoneyGettor.cs
@@ -1,14 +1,30 @@
+using Member.Kimyongmin._02.Code.Agent;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TestMoneyGettor : MonoBehaviour
 {
+    HealthSystem healthSystem;
+    private void Awake()
+    {
+        healthSystem = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthSystem>();
+    }
 
+    private void Start()
+    {
+        healthSystem.SetHealth(100f);
+    }
     void Update()
     {
-        if (Keyboard.current.gKey.wasPressedThisFrame)
+        if(healthSystem == null)
         {
-            CurrencyManager.Instance.AddGold(10);
+            Debug.Log("HealthSystem is null");
+        }
+        if (Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            healthSystem.GetDamage(10f);
+            Debug.Log(healthSystem.Health);
+
         }
     }
 }
--- Assets/Member/YHW/04.Scripts/UI 1/HealthBar.cs
+++ Assets/Member/YHW/04.Scripts/UI 1/HealthBar.cs
@@ -1,16 +1,69 @@
+using Member.Kimyongmin._02.Code.Agent;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Image healthBarImage;
+
+    HealthSystem healthSystem;
+
+    Coroutine barAnimationCoroutine;
+
+    private void Awake()
     {
-        
+        healthBarImage = GetComponent<Image>();
+        healthSystem = GameObject.Find("Player").GetComponent<HealthSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+       
+
+        healthSystem.OnHealthChanged += UpdateBar;
+    }
+
+    private void UpdateBar()
+    {
+        StartCoroutine(UpdateAndAnimateBar());
+    }
+
+    private IEnumerator UpdateAndAnimateBar()
+    {
+        yield return null;
+
+        float targetFill = healthSystem.Health / 100f;
+
+        if (barAnimationCoroutine != null)
+        {
+            StopCoroutine(barAnimationCoroutine);
+        }
+        barAnimationCoroutine = StartCoroutine(AnimateHealthBar(targetFill));
+    }
+    private IEnumerator AnimateHealthBar(float targetFill)
+    {
+        float startFill = healthBarImage.fillAmount; 
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 0.1f)
+        {
+            float t = elapsedTime / 0.1f;
+
+            healthBarImage.fillAmount = Mathf.Lerp(startFill, targetFill, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null; 
+        }
+        Debug.Log(healthBarImage.fillAmount);
+
+        healthBarImage.fillAmount = targetFill;
+    }
+    private void OnDestroy()
+    {
+        if (healthSystem != null)
+        {
+            healthSystem.OnHealthChanged -= UpdateBar;
+        }
     }
 }
