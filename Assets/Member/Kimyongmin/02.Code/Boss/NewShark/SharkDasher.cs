d3ee3fa9e65e90b265e30739469da559c3655601
kimyongmin519 <qwdfbns5198@gmail.com>
2025-11-21 11:24:18 +0900

    etc

---
+++ Assets/Member/Kimyongmin/02.Code/Boss/NewShark/SharkDasher.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Member.Kimyongmin._02.Code.Boss.NewShark
+{
+    public class SharkDasher : MonoBehaviour
+    {
+        public IEnumerator ChargeAttack(Transform self, Vector3 target, SharkMovement sharkMovement)
+        {
+            
+        }
+    }
+}
